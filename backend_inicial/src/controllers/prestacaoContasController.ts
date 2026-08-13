9acf62cef8b1065181e7602569438bc7b6aee693
Bruno Manica <bbmanica@gmail.com>
2026-08-12 22:11:29 -0300

    Feat: adicao de swagger

--- backend_inicial/src/controllers/prestacaoContasController.ts
+++ backend_inicial/src/controllers/prestacaoContasController.ts
@@ -1,22 +1,57 @@
-import { Response, Request } from "express";
+import { Response, Request } from 'express';
+import { ConsultaContas, DadosContaReceber } from '../types/prestacaoConta';
+import {
+  criarConta,
+  listarContas,
+  atualizarConta,
+  excluirConta,
+} from '../services/contasReceberService';
 
-
-interface prestacaoConta {
-    nomeGasto: string,
-    data: string,
-    valor: number,
-    arquivo: string
+interface ParametrosId {
+  id: String;
 }
 
-export function salvar(
-    requisicao: Request<object, object, prestacaoConta>,
-    resposta: Response) {
-    const prestacaoConta = requisicao.body;
-    console.log(prestacaoConta)
+export function criar(
+  requisicao: Request<object, object, DadosContaReceber>,
+  resposta: Response,
+) {
+  const prestacaoConta = requisicao.body;
+  const retorno = criarConta(prestacaoConta);
 
-    resposta.status(201).json({
-        mensagem: "conta criada com sucesso"
-    })
+  resposta.status(201).json(retorno);
+}
 
+export function listar(
+  requisicao: Request<object, object, object, ConsultaContas>,
+  resposta: Response,
+) {
+  const { empresa, status } = requisicao.query;
+  const contas = listarContas(empresa, status);
+  resposta.status(200).json(contas);
+}
 
+export function atualizar(
+  requisicao: Request<ParametrosId, object, DadosContaReceber>,
+  resposta: Response,
+) {
+  const conta = atualizarConta(requisicao.params.id, requisicao.body);
+
+  if (conta) {
+    resposta.status(200).json({ mensagem: 'sucesso ao atualizar' })
+    return
+  }
+
+  resposta.status(500).json({ mensagem: 'Erro ao atualizar' });
+}
+
+export function excluir(
+  requisicao: Request<ParametrosId, object, object>,
+  resposta: Response,
+) {
+  if (excluirConta(requisicao.params.id)) {
+    resposta.status(200).json({ mensagem: 'sucesso ao excluir' });
+    return;
+  }
+
+  resposta.status(500).json({ mensagem: 'Erro ao excluir' });
 }
--- backend_inicial/src/servidor.ts
+++ backend_inicial/src/servidor.ts
@@ -1,13 +1,16 @@
-import express from 'express'
+import express from 'express';
 import { rotas } from './routes/router';
-
+import { documentacaoSwagger } from './configuracoes/swagger';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const porta = 3000;
 
-app.use(express.json())
-app.use(rotas)
+app.use(express.json());
+app.use('/documentacao', swaggerUi.serve, swaggerUi.setup(documentacaoSwagger));
+app.use(rotas);
 
-app.listen(porta,()=>{
-    console.log(`servidor rodando em http://localhost:${porta}`)
-})
+app.listen(porta, () => {
+  console.log(`servidor rodando em http://localhost:${porta}`);
+  console.log(`servidor rodando em http://localhost:${porta}/documentacao`);
+});
